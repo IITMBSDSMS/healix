ca4ca9c0c5b78247e115f3bdd6a7f81274d1f6cd
Avnish <avnishverma718@gmail.com>
2026-05-10 13:50:05 +0530

    fix: resolve CartoDB tile URL by removing retina parameter

--- src/components/ui/VehicleMap.tsx
+++ src/components/ui/VehicleMap.tsx
@@ -50,7 +50,7 @@ export default function VehicleMap({ telemetryData }: VehicleMapProps) {
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
       />
       
       {activeMarkers.map((marker: any) => (
